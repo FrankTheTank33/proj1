eabe21a299c173b1cd22b80379b4c585c791a254
Franklin Wright <franklin.k.wright@gmail.com>
2019-04-04 05:15:51 +0000

    Slight changes

--- project.c
+++ project.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-int encrypt_rotate (int m, int k);
+int encrypt_rotate (int message_letter, int k);
 
-int main () {
-    int e;
-    int m=0;
+int main () {    
+    int cipher_letter;
+    int message_letter=0;
     int k=1;
     
     
-    e = encrypt_rotate (m,k);
-    printf("%d", e);
+    cipher_letter = encrypt_rotate (a,k);
+    printf("Cipher letter for %d is %d", a, cipher_letter);
+    
     
 return 0;
 }
 
-int encrypt_rotate (int m, int k) {
-    return (m+k)%26;
+int encrypt_rotate (int message_letter, int k) {
+    return (message_letter +k)%26;
 }
