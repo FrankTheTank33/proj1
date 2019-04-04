0e9ca042386d6ec25b7d4f69e21059c88c57095e
Franklin Wright <franklin.k.wright@gmail.com>
2019-04-04 05:18:05 +0000

    Changes

--- project.c
+++ project.c
@@ -6,10 +6,11 @@ int main () {
     int cipher_letter;
     int message_letter=0;
     int k=1;
+    printf("Enter allocated value for message letter: \n");
+    scanf("%d", &message_letter);
     
-    
-    cipher_letter = encrypt_rotate (a,k);
-    printf("Cipher letter for %d is %d", a, cipher_letter);
+    cipher_letter = encrypt_rotate (message_letter, k);
+    printf("Cipher letter for %d is %d", message_letter, cipher_letter);
     
     
 return 0;
